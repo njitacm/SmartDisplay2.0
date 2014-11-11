b30c90d5898dbd517945ae5d31524f0bd0ea0da7
binoypatel14@gmail.com <binoypatel14@gmail.com>
2014-11-11 18:27:29 -0500

    Fixed height and min and max time

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	$('#calendar').fullCalendar({
 		events : 'https://www.google.com/calendar/feeds/9niid0lakg17i3rblct46bcafo%40group.calendar.google.com/public/basic',
-		defaultView : 'agendaWeek'
+		defaultView : 'agendaWeek',
+		minTime : '08:00:00',
+		maxTime : '18:00:00',
+		height : 550
 	});
 });
