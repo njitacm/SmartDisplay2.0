6cb1762f47e0548646d3946e6938970a9c90720a
unknown <Administrator@ACMNJIT.njitdm.campus.njit.edu>
2014-11-12 18:05:19 -0500

    Fixed public path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
 app
 	.use(express.static(__dirname + '/public/'))
 	.get('*', function(req, res){
-		res.sendFile('/public/index.html');
+		res.sendFile('public/index.html', {root : __dirname});
 	})
 	.listen(3000);
 
